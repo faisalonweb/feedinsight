02c213d7b59654b6ef7f4e7269fdb5c91df53705
Faisal Usman <p146016@nu.edu.pk>
2020-04-09 12:49:56 +0500

    Add files via upload
    First Commit

---
+++ stack/ThirdViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ThirdViewController.swift
+//  stack
+//
+//  Created by Mac 2014 on 08/04/2020.
+//  Copyright © 2020 faisal. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    
+    @IBOutlet weak var rad: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rad.layer.cornerRadius = 15
+        
+    }
+    
+
+   
+
+}
