76b10cce3710e9e338d2548c1dc6c183a29587f9
Faisal Usman <faisalusman390@gmail.com>
2020-04-20 18:47:40 +0500

    Commit with collectionview

---
+++ feedinsight/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  FeedInsight
+//
+//  Created by Mac 2014 on 20/04/2020.
+//  Copyright © 2020 faisal. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textView: UILabel!
+    
+    
+}
