dc634984c51c5988e3a2298a52484e92e3f11900
Farid Afzal <fareedrao7890@gmail.com>
2020-04-28 11:15:00 +0500

    signup screen constraints set

--- feedinsight/AppDelegate.swift
+++ feedinsight/AppDelegate.swift
@@ -16,25 +16,27 @@ var window:UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // this is testing testing
-        let mainViewController = SigninFscreenViewController()
-               mainViewController.title = "Main"
+//        let mainViewController = SigninFscreenViewController()
+//               mainViewController.title = "Main"
 
-               let frame = UIScreen.main.bounds
-               window = UIWindow(frame: frame)
-
-               window!.rootViewController = mainViewController
-               window!.makeKeyAndVisible()
+//               let frame = UIScreen.main.bounds
+//               window = UIWindow(frame: frame)
+//
+//               window!.rootViewController = mainViewController
+//               window!.makeKeyAndVisible()
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
