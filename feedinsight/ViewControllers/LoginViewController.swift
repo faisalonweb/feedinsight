b284435c803eb076c7fad65f1b44b90e1e47348b
Farid Afzal <fareedrao7890@gmail.com>
2020-05-04 22:03:05 +0500

    activelabel is implemented in signin screen

--- feedinsight/ViewControllers/LoginViewController.swift
+++ feedinsight/ViewControllers/LoginViewController.swift
@@ -7,16 +7,48 @@
 //
 
 import UIKit
-
+ import ActiveLabel
 class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var signupBtn: ActiveLabel!
     @IBOutlet weak var paswordField: UITextField!
     @IBOutlet weak var emailField: UITextField!
+    func SignupSelection(){
+        print("yesss i cameeee")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let signUpViewController = storyboard.instantiateViewController(withIdentifier: "SignupVC") as! SignUpViewController
+        self.present(signUpViewController, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.paswordField.delegate = self
         self.emailField.delegate = self
 
+        let customType = ActiveType.custom(pattern: "\\sSign\\sUp") //Looks for "are"
+                       signupBtn.enabledTypes.append(customType)
+                       signupBtn.urlMaximumLength = 61
+                       
+                       signupBtn.customize { label in
+                       signupBtn.text = "Don't have an Account? Sign Up"
+                           signupBtn.numberOfLines = 1
+                           signupBtn.lineSpacing = 4
+                           signupBtn.customColor[customType] = UIColor(red: 81/255, green: 23/255, blue: 79/255, alpha: 1.0)
+                           signupBtn.customSelectedColor[customType] = UIColor.black
+                   
+                           //
+                           signupBtn.configureLinkAttribute = { (type, attributes, isSelected) in
+                               var atts = attributes
+                               switch type {
+                               case customType:
+                                   atts[NSAttributedString.Key.font] = isSelected ? UIFont.boldSystemFont(ofSize: 12) : UIFont.boldSystemFont(ofSize: 12)
+                               default: ()
+                               }
+                               
+                               return atts
+                           }
+                        label.handleCustomTap(for: customType) { _ in self.SignupSelection() }
+                       }
         // Do any additional setup after loading the view.
     }
     
@@ -24,6 +56,11 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         self.view.endEditing(true)
         return false
     }
+    func alert(_ title: String, message: String) {
+                   let vc = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+                   vc.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+                   present(vc, animated: true, completion: nil)
+               }
 
 //    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
 //        textField.resignFirstResponder()
--- feedinsight/ViewControllers/SignUpViewController.swift
+++ feedinsight/ViewControllers/SignUpViewController.swift
@@ -1,152 +1,157 @@
-   //
-    //  SignUpViewController.swift
-    //  FeedInsight
     //
-    //  Created by Mac on 13/04/2020.
-    //  Copyright © 2020 faisal. All rights reserved.
-    //
-    
-    import UIKit
-    import iOSDropDown
-   import ActiveLabel
-   
-    class SignUpViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+        //  SignUpViewController.swift
+        //  FeedInsight
+        //
+        //  Created by Mac on 13/04/2020.
+        //  Copyright © 2020 faisal. All rights reserved.
+        //
+        
+        import UIKit
+        import iOSDropDown
+        import ActiveLabel
        
-        
-        
-        @IBOutlet weak var haveAccountLabel: ActiveLabel!
-        @IBOutlet weak var NameField: UITextField!
-        @IBOutlet weak var emailField: UITextField!
-        @IBOutlet weak var countryCodeField: UITextField!
-        @IBOutlet weak var dropDownLabel: UIImageView!
-        @IBOutlet weak var phoneField: UITextField!
-        @IBOutlet weak var collectionView: UICollectionView!
-        //    @IBOutlet weak var collectionCell: UICollectionViewCell!
-        @IBOutlet weak var IndustryField: UITextField!
-        @IBOutlet weak var businessField: UITextField!
-        @IBOutlet weak var passwordField: UITextField!
-        @IBOutlet weak var repasswordField: UITextField!
-        @IBOutlet weak var pickanimalfield: DropDown!
-        @IBOutlet weak var pickroleField: DropDown!
-        let textArr = ["Research","Farming","FoodManufacturing"]
-        
-//        let imageArr = [UIImage (named: "brush"), UIImage(named: "brush"),UIImage (named: "brush"), UIImage(named: "brush"),UIImage(named: "brush"),UIImage(named: "brush")]   // research-selected , farm-selected , feedmanufacturing-selected  ,
-        
-        let imageArr: [UIImage] = [
-            UIImage(named: "research-unselected")!,
-            UIImage(named: "farm-unselected")!,
-            UIImage(named: "feedmanufacturing-unselected")!,
-        ]
-        let imageArr1: [UIImage] = [
-                   UIImage(named: "research-selected")!,
-                   UIImage(named: "farm-selected")!,
-                   UIImage(named: "feedmanufacturing-selected")!,
-               ]
-        
-        @IBAction func backBtn(_ sender: UIButton) {
-            self.dismiss(animated: true, completion: nil)
-        }
-        @IBAction func signIntapped(_ sender: UIButton) {
-        }
-        
-        @IBAction func signInBtntap(_ sender: Any) {
-        }
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            collectionView.delegate = self
-            collectionView.dataSource = self
-            let itemSize = UIScreen.main.bounds.width/3 - 2
+        class SignUpViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+           
             
-            let layout = UICollectionViewFlowLayout()
-            layout.scrollDirection = .horizontal
-            layout.itemSize = CGSize(width: itemSize, height: itemSize)
             
-            layout.minimumInteritemSpacing = 2
-            layout.minimumLineSpacing = 16
+            @IBOutlet weak var haveAccountLabel: ActiveLabel!
+            @IBOutlet weak var NameField: UITextField!
+            @IBOutlet weak var emailField: UITextField!
+            @IBOutlet weak var countryCodeField: UITextField!
+            @IBOutlet weak var dropDownLabel: UIImageView!
+            @IBOutlet weak var phoneField: UITextField!
+            @IBOutlet weak var collectionView: UICollectionView!
+            //    @IBOutlet weak var collectionCell: UICollectionViewCell!
+            @IBOutlet weak var IndustryField: UITextField!
+            @IBOutlet weak var businessField: UITextField!
+            @IBOutlet weak var passwordField: UITextField!
+            @IBOutlet weak var repasswordField: UITextField!
+            @IBOutlet weak var pickanimalfield: DropDown!
+            @IBOutlet weak var pickroleField: DropDown!
+            let textArr = ["Research","Farming","FoodManufacturing"]
             
-            collectionView.collectionViewLayout = layout
+    //        let imageArr = [UIImage (named: "brush"), UIImage(named: "brush"),UIImage (named: "brush"), UIImage(named: "brush"),UIImage(named: "brush"),UIImage(named: "brush")]   // research-selected , farm-selected , feedmanufacturing-selected  ,
             
-            pickanimalfield.optionArray = ["Option 1", "Option 2", "Option 3"]
-            pickanimalfield.optionIds = [1,23,54,22]
-            pickanimalfield.didSelect{(selectedText , index ,id) in
+            let imageArr: [UIImage] = [
+                UIImage(named: "research-unselected")!,
+                UIImage(named: "farm-unselected")!,
+                UIImage(named: "feedmanufacturing-unselected")!,
+            ]
+            let imageArr1: [UIImage] = [
+                       UIImage(named: "research-selected")!,
+                       UIImage(named: "farm-selected")!,
+                       UIImage(named: "feedmanufacturing-selected")!,
+                   ]
+            
+            @IBAction func backBtn(_ sender: UIButton) {
+                self.dismiss(animated: true, completion: nil)
             }
-            pickroleField.optionArray = ["Option 1", "Option 2", "Option 3"]
-            pickroleField.optionArray = ["Admin","user","manager"]
-            pickroleField.didSelect{(selectedText , index ,id) in
+            @IBAction func signIntapped(_ sender: UIButton) {
             }
-            //
             
-             let customType = ActiveType.custom(pattern: "\\sSign\\sIn") //Looks for "are"
-            haveAccountLabel.enabledTypes.append(customType)
-            haveAccountLabel.urlMaximumLength = 61
+            @IBAction func signInBtntap(_ sender: Any) {
+            }
             
-            haveAccountLabel.customize { label in
-            haveAccountLabel.text = "Have an Account? Sign In"
-                label.numberOfLines = 1
-                label.lineSpacing = 4
-                label.customColor[customType] = UIColor(red: 81/255, green: 23/255, blue: 79/255, alpha: 1.0)
-                label.customSelectedColor[customType] = UIColor.black
-        
-                //
-                label.configureLinkAttribute = { (type, attributes, isSelected) in
-                    var atts = attributes
-                    switch type {
-                    case customType:
-                        atts[NSAttributedString.Key.font] = isSelected ? UIFont.boldSystemFont(ofSize: 12) : UIFont.boldSystemFont(ofSize: 12)
-                    default: ()
-                    }
-                    
-                    return atts
+            func btnSelection(){
+                print("yesss i cameeee1")
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let loginViewController = storyboard.instantiateViewController(withIdentifier: "SigninVC") as! LoginViewController
+                self.present(loginViewController, animated: true, completion: nil)
+            }
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                collectionView.delegate = self
+                collectionView.dataSource = self
+                let itemSize = UIScreen.main.bounds.width/3 - 2
+                
+                let layout = UICollectionViewFlowLayout()
+                layout.scrollDirection = .horizontal
+                layout.itemSize = CGSize(width: itemSize, height: itemSize)
+                
+                layout.minimumInteritemSpacing = 2
+                layout.minimumLineSpacing = 16
+                
+                collectionView.collectionViewLayout = layout
+                
+                pickanimalfield.optionArray = ["Option 1", "Option 2", "Option 3"]
+                pickanimalfield.optionIds = [1,23,54,22]
+                pickanimalfield.didSelect{(selectedText , index ,id) in
+                }
+                pickroleField.optionArray = ["Option 1", "Option 2", "Option 3"]
+                pickroleField.optionArray = ["Admin","user","manager"]
+                pickroleField.didSelect{(selectedText , index ,id) in
                 }
                 //
                 
-                label.handleCustomTap(for: customType) { self.alert("Custom type", message: $0) }
+                 let customType = ActiveType.custom(pattern: "\\sSign\\sIn") //Looks for "are"
+                haveAccountLabel.enabledTypes.append(customType)
+                haveAccountLabel.urlMaximumLength = 61
+                
+                haveAccountLabel.customize { label in
+                haveAccountLabel.text = "Have an Account? Sign In"
+                    haveAccountLabel.numberOfLines = 1
+                    haveAccountLabel.lineSpacing = 4
+                    haveAccountLabel.customColor[customType] = UIColor(red: 81/255, green: 23/255, blue: 79/255, alpha: 1.0)
+                    haveAccountLabel.customSelectedColor[customType] = UIColor.black
+            
+                    //
+                    haveAccountLabel.configureLinkAttribute = { (type, attributes, isSelected) in
+                        var atts = attributes
+                        switch type {
+                        case customType:
+                            atts[NSAttributedString.Key.font] = isSelected ? UIFont.boldSystemFont(ofSize: 12) : UIFont.boldSystemFont(ofSize: 12)
+                        default: ()
+                        }
+                        
+                        return atts
+                    }
+                    haveAccountLabel.handleCustomTap(for: customType) { _ in self.btnSelection() }
+                }
+                
             }
             
-        }
-        
-        func alert(_ title: String, message: String) {
-            let vc = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-            vc.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
-            present(vc, animated: true, completion: nil)
-        }
+            func alert(_ title: String, message: String) {
+                let vc = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+                vc.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+                present(vc, animated: true, completion: nil)
+            }
 
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-                   return textArr.count
-               }
-               
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-                   let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! SignUpCollectionViewCell
-                   let cellIndex = indexPath.item
-                   cell.industryImage.image = imageArr[cellIndex]
-                   cell.industryLabel.text = textArr[cellIndex]
-                   return cell
-               }
-        
-        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-            let cell = collectionView.cellForItem(at: indexPath) as! SignUpCollectionViewCell
-             let cellIndex = indexPath.item
-            cell.industryImage.image = imageArr1[cellIndex]
+            func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+                       return textArr.count
+                   }
+                   
+            func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath) as! SignUpCollectionViewCell
+                       let cellIndex = indexPath.item
+                       cell.industryImage.image = imageArr[cellIndex]
+                       cell.industryLabel.text = textArr[cellIndex]
+                       return cell
+                   }
             
-        
+            func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+                let cell = collectionView.cellForItem(at: indexPath) as! SignUpCollectionViewCell
+                 let cellIndex = indexPath.item
+                cell.industryImage.image = imageArr1[cellIndex]
+                
+            
+            }
+            func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+                let cell = collectionView.cellForItem(at: indexPath) as! SignUpCollectionViewCell
+                        let cellIndex = indexPath.item
+                        cell.industryImage.image = imageArr[cellIndex]
+            }
+            
+            
+            /*
+             
+             // MARK: - Navigation
+             
+             // In a storyboard-based application, you will often want to do a little preparation before navigation
+             override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+             // Get the new view controller using segue.destination.
+             // Pass the selected object to the new view controller.
+             }
+             */
+            
         }
-        func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-            let cell = collectionView.cellForItem(at: indexPath) as! SignUpCollectionViewCell
-                    let cellIndex = indexPath.item
-                    cell.industryImage.image = imageArr[cellIndex]
-        }
-        
-        
-        /*
-         
-         // MARK: - Navigation
-         
-         // In a storyboard-based application, you will often want to do a little preparation before navigation
-         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-         }
-         */
-        
-    }
 
