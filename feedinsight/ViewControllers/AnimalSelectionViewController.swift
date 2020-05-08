a141f9bf1e316c58dd27d6ab154b3f456d0fc118
Farid Afzal <fareedrao7890@gmail.com>
2020-05-08 16:49:22 +0500

    cet constraints for pick lebel and select animal

--- feedinsight/ViewControllers/AnimalSelectionViewController.swift
+++ feedinsight/ViewControllers/AnimalSelectionViewController.swift
@@ -48,7 +48,7 @@ class AnimalSelectionViewController: UIViewController,UICollectionViewDataSource
         let itemSize = UIScreen.main.bounds.width/3 - 2
         
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: itemSize + 30, height: itemSize + 30)
+        layout.itemSize = CGSize(width: itemSize + 15, height: itemSize + 15)
         
         layout.minimumInteritemSpacing = 2
         layout.minimumLineSpacing = 20
