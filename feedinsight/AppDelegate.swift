99feb867ee07c24fb97d2c4f24a41bc949e6b90a
Farid Afzal <fareedrao7890@gmail.com>
2020-05-09 13:38:11 +0500

    dark model opt out, signup screen fields font size decreased

--- feedinsight/AppDelegate.swift
+++ feedinsight/AppDelegate.swift
@@ -17,6 +17,9 @@ var window:UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
         // Override point for customization after application launch.
         // this is testing testing
 //        let mainViewController = SigninFscreenViewController()
