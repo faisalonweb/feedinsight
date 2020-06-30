ecc6d4c84413fc0d7518c8f8b09a165b3f703796
Faisal Usman <faisalusman390@gmail.com>
2020-06-30 08:38:13 +0500

    water analysis first screen

---
+++ feedinsight/wateroneViewController.swift
@@ -0,0 +1,44 @@
+//
+//  wateroneViewController.swift
+//  FeedInsight
+//
+//  Created by Mac 2014 on 30/06/2020.
+//  Copyright © 2020 faisal. All rights reserved.
+//
+
+import UIKit
+
+class wateroneViewController: UIViewController {
+
+    @IBOutlet weak var profileimage: UIImageView!
+    @IBOutlet weak var standardbtn: UIButton!
+    @IBOutlet weak var custombtn: UIButton!
+    @IBOutlet weak var palabel: UILabel!
+    @IBOutlet weak var calabel: UILabel!
+    @IBOutlet weak var mglabel: UILabel!
+    @IBOutlet weak var klabel: UILabel!
+    @IBOutlet weak var nalabel: UILabel!
+    @IBOutlet weak var cllabel: UILabel!
+    @IBOutlet weak var slabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        
+        profileimage.layer.cornerRadius = 17
+        standardbtn.layer.cornerRadius = 8
+        custombtn.layer.cornerRadius = 8
+        palabel.layer.cornerRadius = 15
+        calabel.layer.cornerRadius = 15
+        mglabel.layer.cornerRadius = 15
+        klabel.layer.cornerRadius = 15
+        nalabel.layer.cornerRadius = 15
+        calabel.layer.cornerRadius = 15
+        slabel.layer.cornerRadius = 15
+        super.viewDidLoad()
+
+       
+    }
+    
+
+   
+}
