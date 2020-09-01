8f2c51b5492b82575dcf83a5f24d2e77b384dc7f
Faisal Usman <faisalusman390@gmail.com>
2020-09-01 09:06:15 +0500

    Latest push

---
+++ feedinsight/Model/Person.swift
@@ -0,0 +1,38 @@
+//
+//  Person.swift
+//  Task7
+//
+//  Created by Eddie Lui on 14/5/19.
+//  Copyright © 2019 Eddie Lui. All rights reserved.
+//
+
+import Foundation
+
+struct Person : Codable {
+    var Type1: String
+    var FeedName: String
+    var DryMatter: Double
+    var Ca:Double
+    var CaAbs:Double
+    var P: Double
+    var PAbs: Double
+    var Mg: Double
+    var MgAbs: Double
+    var K: Double
+    var S: Double
+    var Na: Double
+    var Cl: Double
+    var Fe: Double
+    var Zn: Double
+    var Cu: Double
+    var Mn: Double
+    var Se: Double
+    var Co: String
+    var I: String
+    var VitaminA: String
+    var VitaminD3: String
+    var VitaminE: String
+    var Niacin: String
+    var Biotin : String
+    var Reference: String
+}
