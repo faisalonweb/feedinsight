dbd78226d96d73954afc84e714db266a716081cf
Farid Afzal <fareedrao7890@gmail.com>
2020-10-12 12:04:29 +0500

    database screen added

---
+++ feedinsight/Controller/DatabaseViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DatabaseViewController.swift
+//  FeedInsight
+//
+//  Created by Hamza Iqbal on 12/10/2020.
+//  Copyright © 2020 faisal. All rights reserved.
+//
+
+import UIKit
+
+class DatabaseViewController: UIViewController {
+
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backBtnTapped(_ sender: Any) {
+        
+    }
+    @IBAction func doneBtnTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ feedinsight/View/loadDatabaseTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  loadDatabaseTableViewCell.swift
+//  FeedInsight
+//
+//  Created by Hamza Iqbal on 12/10/2020.
+//  Copyright © 2020 faisal. All rights reserved.
+//
+
+import UIKit
+
+class loadDatabaseTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
